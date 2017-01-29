d23a5f02638986ee6cd2ed55fcdf2b12a76d5e0f
markmathews <mm2121@cam.ac.uk>
2017-01-29 07:00:23 +0000

    Worked on post.py

--- post.py
+++ post.py
@@ -4,18 +4,13 @@ import requests
 #accept input from sensors 
 #TO BE DONE 
 
-print("Enter Use Mode")
-print("Training - enter 'a'")
-print("Run application - enter 'b'")
-
-use_mode = input(); 
-
-if use_mode == 'a': 
-	payload = {'key1': 'value1', 'key2': 'value2'}
-	r = requests.get('http://httpbin.org/get', params=payload)
-	print(r.url)
-	#TO BE CONT.  
-elif use_mode == 'b': 
-	#More code... 
-else: 
-	print("Error: choose either a or b") 
+def make_request(mode): 
+	if mode == 'run': 
+		payload = {'key1': 'value1', 'key2': 'value2'}
+		r = requests.get('http://httpbin.org/get', params=payload)
+		print(r.url)
+		#TO BE CONT.  
+	elif mode == 'learn': 
+		#More code... 
+	else: 
+		print("Error: Incorrect input to function 'make_request'. Choose either 'run' or 'train' as input") 
