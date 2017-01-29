5579d3b19869c9c799f43715acda21a777e300f5
markmathews <mm2121@cam.ac.uk>
2017-01-29 08:17:02 +0000

    Post requests working

--- post.py
+++ post.py
@@ -6,16 +6,16 @@ import requests
 
 def make_request(mode): 
 	payload = {'key1': 'value1', 'key2': 'value2'}
-	r = post('http://httpbin.org/get', params=payload)
+	r = requests.post('http://httpbin.org/get', params=payload)
 	print(r.url)
 	#TO BE CONT.  
 
 	if mode == 'run':
-		print("run")
+		print('run')
 	elif mode == 'learn': 
 		#More code... else: 
 		print('learn')
-	print("Error: Incorrect input to function 'make_request'. Choose either 'run' or 'train' as input") 
+	else: 
+		print("Error: Incorrect input to function 'make_request'. Choose either 'run' or 'train' as input") 
 
 
-make_request('run')
